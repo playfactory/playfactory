af961d329720ffc5374c4eb0a284050f3be0f439
Welson Play <welsonplayqs@gmail.com>
2017-03-29 16:50:54 -0300

    melhorias

--- src/PlayFactory/Events/Bus/EventBusModule.cs
+++ src/PlayFactory/Events/Bus/EventBusModule.cs
@@ -1,12 +1,5 @@
-﻿using System.Linq;
-using System.Reflection;
-using Autofac;
-using Autofac.Core;
-using Microsoft.Extensions.Logging;
-using PlayFactory.IoC;
-using PlayFactory.Logger;
+﻿using Autofac;
 using PlayFactory.Modules;
-using PlayFactory.Reflection.Extensions;
 
 namespace PlayFactory.Events.Bus
 {
@@ -20,37 +13,6 @@ namespace PlayFactory.Events.Bus
                 .SingleInstance();
 
             base.Load(builder);
-        }
-
-        private static void InjectLoggerProperties(object instance)
-        {
-            var instanceType = instance.GetType();
-
-            if (!(instance is IResolveIoCInstance))
-                return;
-
-            // Get all the injectable properties to set.
-            // If you wanted to ensure the properties were only UNSET properties,
-            // here's where you'd do it.
-            var properties = instanceType
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .Where(p => p.PropertyType == typeof(ILogger) && p.CanWrite && p.GetIndexParameters().Length == 0);
-
-            // Set the properties located.
-            foreach (var propToSet in properties)
-            {
-                propToSet.SetValue(instance, LogManager.GetLogger(instanceType), null);
-            }
-        }
-
-    
-
-        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry,
-            IComponentRegistration registration)
-        {
-
-            // Handle properties.
-            registration.Activated += (sender, e) => InjectLoggerProperties(e.Instance);
         }
     }
 }
--- src/PlayFactory/PlayFactoryBaseModule.cs
+++ src/PlayFactory/PlayFactoryBaseModule.cs
@@ -1,5 +1,11 @@
-﻿using Autofac;
+﻿using System.Linq;
+using System.Reflection;
+using Autofac;
+using Autofac.Core;
+using Microsoft.Extensions.Logging;
+using PlayFactory.IoC;
 using PlayFactory.IoC.Extensions;
+using PlayFactory.Logger;
 using PlayFactory.Modules;
 using PlayFactory.Reflection.Extensions;
 
@@ -17,6 +23,37 @@ namespace PlayFactory
         public override bool AutoLoad()
         {
             return false;
+        }
+
+        private static void InjectLoggerProperties(object instance)
+        {
+            var instanceType = instance.GetType();
+
+            if (!(instance is IResolveIoCInstance))
+                return;
+
+            // Get all the injectable properties to set.
+            // If you wanted to ensure the properties were only UNSET properties,
+            // here's where you'd do it.
+            var properties = instanceType
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(p => p.PropertyType == typeof(ILogger) && p.CanWrite && p.GetIndexParameters().Length == 0);
+
+            // Set the properties located.
+            foreach (var propToSet in properties)
+            {
+                propToSet.SetValue(instance, LogManager.GetLogger(instanceType), null);
+            }
+        }
+
+
+
+        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry,
+            IComponentRegistration registration)
+        {
+
+            // Handle properties.
+            registration.Activated += (sender, e) => InjectLoggerProperties(e.Instance);
         }
     }
 }
